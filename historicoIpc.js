dddc7b62528aceb34c5da6bec1b421001d5e5ba0
sebastianMartinez25 <sebastianmartinez25065@gmail.com>
2022-05-30 18:57:11 -0500

    version 1.0

---
+++ historicoIpc.js
@@ -0,0 +1,33 @@
+const vue = new Vue({
+ el:'#app',
+ data:{
+  listaDatos : []
+ },
+ created(){
+  this.getLista()
+ },
+ methods:{
+  recargar(){
+   console.log("recargando");
+   this.getLista()
+  },
+getLista(){
+ // id de la hoja de calculo
+ idSheets = '1nR5OPyrQECTZ2CTUB7rclW6VYJlR-7TOUsKBMym09a4';
+ //// nuestra      APIKey
+ apiKey = 'AIzaSyD4TFfvj5gmSy1ndPFA9GxO8LOulEyk-ik'; 
+ // rango de la hoja de calculo que queremos leer
+ values = 'A2:E10000';
+// fetch es un método nativo para hacer peticiones http
+// en el navegador 
+ fetch("https://content-sheets.googleapis.com/v4/spreadsheets/" +   idSheets + "/values/A2:E10000?access_token="+ apiKey +"&key="+  apiKey)
+.then((lista)=>{
+  return lista.json()
+}).then((valores)=>{
+  this.listaDatos = valores.values
+}).catch(err=>{
+  console.log(err);
+})
+} // fin funcion getLista()
+} // fin methods
+}) // fin instancia
