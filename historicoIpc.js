dfe5a12bc36d9d9f964367f2d9e57360278436b2
sebastianMartinez25 <sebastianmartinez25065@gmail.com>
2022-05-31 21:46:31 -0500

    Version 3.0

--- historicoIpc.js
+++ historicoIpc.js
@@ -25,6 +25,19 @@ getLista(){
   return lista.json()
 }).then((valores)=>{
   this.listaDatos = valores.values.reverse();
+  //CODIGO NUEVO
+var filaInicial=0;
+var valorAño=(this.listaDatos[filaInicial][0]);
+var valorMes=(this.listaDatos[filaInicial][1]).toLowerCase();
+var valorIpcMensual=(this.listaDatos[filaInicial][2]);
+var valorIpcAnual=(this.listaDatos[filaInicial][3]);
+var valorIpcCorrido=(this.listaDatos[filaInicial][4]);
+var datosActuales=document.getElementById("datosActuales");
+var mensaje =document.createTextNode("En " + valorMes + " de " + valorAño + ", la variación mensual del IPC fue " 
++ valorIpcMensual + "%," + " la variación año corrido fue " + valorIpcCorrido +"%, y la anual fue "
++ valorIpcAnual +"%.");
+datosActuales.appendChild(mensaje);
+  //FIN CODIGO NUEVO
 }).catch(err=>{
   console.log(err);
 })
